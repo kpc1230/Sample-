a4211319b71ccd7e5fc562729c48b9ea6ff007e3
Aleksey Matsarski <alex.matsarski@getzephyr.com>
2017-08-08 17:27:56 -0700

    Spring boot initial code

---
+++ src/main/java/com/thed/zephyr/cloud/capture/AddonApplication.java
@@ -0,0 +1,12 @@
+package com.thed.zephyr.cloud.capture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddonApplication {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(AddonApplication.class).run(args);
+    }
+}
