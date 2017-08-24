10bf511b2562d820ef5c5db62ab64bed189c77fb
snurulla <syed.nurulla@getzephyr.com>
2017-08-24 18:02:29 +0530

    Added Customfield model

---
+++ src/main/java/com/thed/zephyr/capture/model/jira/CustomField.java
@@ -0,0 +1,101 @@
+package com.thed.zephyr.capture.model.jira;
+
+/**
+ * Created by snurulla on 8/24/2017.
+ */
+public class CustomField {
+    private Schema schema;
+    private String id;
+
+    private String orderable;
+
+    private String[] clauseNames;
+
+    private String name;
+
+    private String navigable;
+
+    private String searchable;
+
+    private String key;
+
+    private String custom;
+
+    public Schema getSchema() {
+        return schema;
+    }
+
+    public void setSchema(Schema schema) {
+        this.schema = schema;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderable() {
+        return orderable;
+    }
+
+    public void setOrderable(String orderable) {
+        this.orderable = orderable;
+    }
+
+    public String[] getClauseNames
+            () {
+        return clauseNames
+                ;
+    }
+
+    public void setClauseNames
+            (String[] clauseNames
+            ) {
+        this.clauseNames
+                = clauseNames
+        ;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNavigable() {
+        return navigable;
+    }
+
+    public void setNavigable(String navigable) {
+        this.navigable = navigable;
+    }
+
+    public String getSearchable() {
+        return searchable;
+    }
+
+    public void setSearchable(String searchable) {
+        this.searchable = searchable;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+}
---
+++ src/main/java/com/thed/zephyr/capture/model/jira/Schema.java
@@ -0,0 +1,37 @@
+package com.thed.zephyr.capture.model.jira;
+
+/**
+ * Created by snurulla on 8/24/2017.
+ */
+public class Schema {
+    private String customId;
+
+    private String type;
+
+    private String custom;
+
+    public String getCustomId() {
+        return customId;
+    }
+
+    public void setCustomId(String customId) {
+        this.customId = customId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+
+}
