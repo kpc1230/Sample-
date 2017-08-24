32409a5e47ab9f33af2a16d08e8c3c8591e45024
snurulla <syed.nurulla@getzephyr.com>
2017-08-24 11:31:55 +0530

    Missing file added

---
+++ src/main/java/com/thed/zephyr/capture/util/PlainRestTemplate.java
@@ -0,0 +1,11 @@
+package com.thed.zephyr.capture.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by snurulla on 8/23/2017.
+ */
+@Component
+public class PlainRestTemplate extends RestTemplate {
+}
