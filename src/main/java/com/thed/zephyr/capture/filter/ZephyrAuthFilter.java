137d77a4ba0fd5382982b33082471cf1ad664ffb
snurulla <syed.nurulla@getzephyr.com>
2017-10-25 12:41:48 +0530

    Fixed CAPCLOUD-351

--- src/main/java/com/thed/zephyr/capture/filter/ZephyrAuthFilter.java
+++ src/main/java/com/thed/zephyr/capture/filter/ZephyrAuthFilter.java
@@ -93,10 +93,10 @@ public class ZephyrAuthFilter extends JwtAuthenticationFilter {
                         return false;
                     }
                     AcHostModel acHostModel = acHostModelRepository.findOne(clientKey);
-                    if(null != acHostModel) {
+                    if(null != acHostModel&&acHostModel.getStatus().equals(AcHostModel.TenantStatus.ACTIVE)) {
                         JwtAuthentication jwtAuthentication = new JwtAuthentication(new AtlassianHostUser(acHostModel, Optional.ofNullable(userKey)), new JWTClaimsSet());
                         //Put JwtAuthentication into SecurityContext to mock JwtAuthenticationFilter behavior and allow RequireAuthenticationHandlerInterceptor pass request
-                        SecurityContextHolder.getContext().setAuthentication(jwtAuthentication);
+                       SecurityContextHolder.getContext().setAuthentication(jwtAuthentication);
                         return true;
                     }else {
                         return false;
