83eb83b886a70cc7ff5f1b6c31ed611fd23bab65
Manjunath.K <manjunath.k@getzephyr.com>
2019-02-20 17:57:01 +0530

    Added ignoreJWT annotation to job progress

--- src/main/java/com/thed/zephyr/capture/controller/JobProgressController.java
+++ src/main/java/com/thed/zephyr/capture/controller/JobProgressController.java
@@ -1,21 +1,27 @@
 package com.thed.zephyr.capture.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atlassian.connect.spring.AtlassianHostUser;
+import com.atlassian.connect.spring.IgnoreJwt;
 import com.thed.zephyr.capture.exception.CaptureRuntimeException;
 import com.thed.zephyr.capture.model.AcHostModel;
 import com.thed.zephyr.capture.service.JobProgressService;
+import com.thed.zephyr.capture.service.ac.DynamoDBAcHostRepository;
 import com.thed.zephyr.capture.util.CaptureI18NMessageSource;
+import com.thed.zephyr.capture.util.CaptureUtil;
 
 /**
  * @author manjunath
@@ -34,11 +40,22 @@ public class JobProgressController {
 	@Autowired
 	private CaptureI18NMessageSource captureI18NMessageSource;
 	
+	@Autowired
+	private DynamoDBAcHostRepository dynamoDBAcHostRepository;
+	
+	@IgnoreJwt
 	@GetMapping(value = "/{jobProgressTicket}")
-	public ResponseEntity<?> getJobProgress(@PathVariable String jobProgressTicket, @AuthenticationPrincipal AtlassianHostUser hostUser) {
+	public ResponseEntity<?> getJobProgress(@PathVariable String jobProgressTicket, @RequestParam Optional<String> baseURL) {
 		log.info("Start of getJobProgress() -> params - jobProgressId -> " + jobProgressTicket);
 		try {
-			AcHostModel acHostModel = (AcHostModel) hostUser.getHost();
+			AcHostModel acHostModel = null;
+			if(baseURL.isPresent()) {
+				acHostModel = CaptureUtil.getAcHostModel(dynamoDBAcHostRepository, baseURL.get());
+			} else {
+				Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+				AtlassianHostUser atlassianHostUser = (AtlassianHostUser) auth.getPrincipal();
+	            acHostModel = (AcHostModel) atlassianHostUser.getHost();
+			}
 			Map<String, Object> progressMap = jobProgressService.checkJobProgress(acHostModel, jobProgressTicket);
             if (null == progressMap) {
             	String errorMessage = captureI18NMessageSource.getMessage("capture.job.progress.status.error");
